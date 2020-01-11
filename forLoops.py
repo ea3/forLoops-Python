058f4f611880da4adef53107236fc228e88c8542
Emilio Araos <ea3dev@gmail.com>
2020-01-11 11:58:51 -0700

    printing even numbers on a list

---
+++ forLoops.py
@@ -0,0 +1,8 @@
+my_List = ['Emilio', 'Laura', 'Andrea', 'Tony', 'Ramon']
+for name in my_List:
+	print (name)
+
+my_second_list = [1,2,3,4,5,6,7,8,9,10]
+for number in my_second_list:
+	if (number % 2 == 0 ): 			# check for even. 
+		print(number)				#Prints multiple of 2. 
